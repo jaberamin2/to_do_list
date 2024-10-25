dcf34885415035e99f7b26eaff8005334e41615f
Jaber <jaberamin018@gmail.com>
2024-10-25 23:42:06 +0600

    edit button

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ function addTodo() {
     const todoText = todoInput.value.trim();
     if (!todoText) return;
   
-    // Create list item
     const todoItem = document.createElement("li");
     todoItem.className = "flex justify-between items-center bg-gray-100 p-4 rounded-lg shadow-sm";
   
-    // Create the text span
     const todoContent = document.createElement("span");
     todoContent.className = "flex-1 text-gray-800";
     todoContent.textContent = todoText;
+
+
+    const editBtn = document.createElement("button");
+  editBtn.textContent = "Edit";
+  editBtn.className = "text-indigo-500 hover:underline mr-2";
+  editBtn.addEventListener("click", () => editTodo(todoItem, todoContent));
   
     
   }
